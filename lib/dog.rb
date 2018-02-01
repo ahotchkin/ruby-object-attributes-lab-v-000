07172b9c2670b3d77b840d7ff510b7d15cbd8bd6
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-02-01 01:07:04 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,5 @@
 class Dog
-  
+
   def name= (name)
     @name = name
   end
@@ -8,4 +8,12 @@ class Dog
     @name
   end
 
+  def breed= (breed)
+    @breed = breed
+  end
+
+  def breed
+    @breed
+  end
+  
 end
